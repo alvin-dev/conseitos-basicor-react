30b9dd879387aeeee7041f2683273bcad9512999
Alvaro <alvinweeb@gmail.com>
2021-11-11 19:02:18 -0300

    components condicionais

--- src/app.jsx
+++ src/app.jsx
@@ -6,36 +6,45 @@ import ComParametro from './components/ComParametro'
 import ComFilhos from './components/ComFilhos'
 import Card from './components/layout/card'
 import Repeticao from './components/Repeticao';
-
+import Condicional from './components/Condicional';
+import CondicionalComIf from './components/CondicionalComIf';
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default props => (
-<div className="App">
+    <div className="App">
 
-<Card titulo="#01 Primeiro Componente">
-    <Primeiro/>
-</Card>
+        <Card titulo="#01 Primeiro Componente">
+            <Primeiro />
+        </Card>
 
-<Card titulo="#02 Componente com Parametro">
-    <ComParametro titulo="Opa" subtitulo="Epa" />
-</Card>
+        <Card titulo="#02 Componente com Parametro">
+            <ComParametro titulo="Opa" subtitulo="Epa" />
+        </Card>
 
-<Card titulo="#03 Componente com Filhos">
-    <ComFilhos>
-        <ul>
-        <li>Ana</li>
-        <li>Bia</li>
-        <li>Carlos</li>
-        <li>Daniel</li>
-        </ul>
-    </ComFilhos>
-</Card>
+        <Card titulo="#03 Componente com Filhos">
+            <ComFilhos>
+                <ul>
+                    <li>Ana</li>
+                    <li>Bia</li>
+                    <li>Carlos</li>
+                    <li>Daniel</li>
+                </ul>
+            </ComFilhos>
+        </Card>
 
-<Card titulo="#04 Componente com Repetição">
-    <Repeticao></Repeticao>
-</Card>
+        <Card titulo="#04 Componente com Repetição">
+            <Repeticao></Repeticao>
+        </Card>
+
+        <Card titulo="#05 Condicional v1">
+            <Condicional numero={10}></Condicional>
+        </Card>
+
+        <Card titulo="#06 Condicional v2">
+            <CondicionalComIf numero={11}></CondicionalComIf>
+        </Card>
 
 
 
-</div>
+    </div>
 );
--- src/components/Repeticao.jsx
+++ src/components/Repeticao.jsx
@@ -7,7 +7,7 @@ export default () => {
     return products.map(prod => {
       return <li key={prod.id}>
         {prod.id} - {prod.name} = R$ {prod.price}
-        </li>
+      </li>
     })
   }
 
